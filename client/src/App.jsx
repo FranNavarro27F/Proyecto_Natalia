d8aec760734ec078e05e508b3a0267a3c0911a07
Francisco Navarro <fran27_dev@gmail.com>
2023-09-29 13:21:09 -0300

    QR generator mejorado para test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import QRCode from "react-qr-code";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+    const [textQR, setTextQR] = useState('');
+  
+    const handleInputChange = (e) => {
+      setTextQR(e.target.value);
+    };
+  
 
   return (
     <>
-      <div>
-        {/* Modificar el valor asignado a la propiedad "value=" va a generar un QR diferente con esa informacion metida */}
-        {/* <QRCode value="Francisco Navarro / DNI:38701111 / pariente de: Juan Navarro" /> */}
-        <QRCode value="Perdon, Lei, esto me toma algo de tiempo, no por el programa es si sino las caracteristicas del programa jaja ;)" />
+      <div className='div_cont'>
+        <p>Lector de QR</p>
+        
       </div>
+   
+    <div className='div_cont'>
+      <label htmlFor="text-input">Ingresa el texto:</label>
+      <input
+        type="text"
+        id="text-input"
+        value={textQR}
+        onChange={handleInputChange}
+      />
+      <p></p>
+      {setTextQR && <QRCode value={textQR} size={300} />}
+    </div>
     </>
   )
 }
