9913aae00da7b0c1f07cfcf669c71566b41160db
Francisco Navarro <fran27_dev@gmail.com>
2023-10-07 07:40:59 -0300

    agregando puerto intento 01

--- api/src/app.js
+++ api/src/app.js
@@ -29,5 +29,5 @@ mongoose
 app.use("/api/visitantes", visitantesRouter);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server listening on port 8080");
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
